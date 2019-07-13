ac5b640be55ea5e0b6e741deb65bfcc57744d355
Dariusz Rybicki <darrarski@gmail.com>
2019-07-13 21:09:01 +0200

    Instantiate app UI programmatically instead of using Main.storyboard
    This preserves state restoration logic, so it still works as intended

--- StateRestorationApp/AppDelegate.swift
+++ StateRestorationApp/AppDelegate.swift
@@ -5,8 +5,19 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
+    func application(
+        _ application: UIApplication,
+        willFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
+        ) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.restorationIdentifier = "MainWindow"
+        window?.rootViewController = UIStoryboard(name: "Main", bundle: .main).instantiateInitialViewController()
+        return true
+    }
+
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        window?.makeKeyAndVisible()
         return true
     }
 
